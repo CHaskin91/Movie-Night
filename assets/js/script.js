a3b71207868cb975e79744959e0f4e6b29316b11
Evan Diaz <evand1998@gmail.com>
2022-01-11 16:52:34 -0600

    first cocmmit

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// Set global variables, including Open Weather Maps API Key
+const API = "api_key=tj987UYVa5KOb4Y3OcZWXeYQjtfjn55R";
+const Base_URL = "https://api.internationalshowtimes.com/v4/showtimes?movie_id=18047&location=52.331705,13.37&distance=30"
+const API_URl = 
+var currentMovie = "";
+var lastMovie = "";
