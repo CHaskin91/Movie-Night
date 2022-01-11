a10f612d3c960106f976b8fa9e10806a686265a8
Chubbs91 <chaskin1991@outlook.com>
2022-01-11 00:47:33 -0600

    created el for main. added to show movie function. appending results for movie el

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ const baseURL = 'https://api.themoviedb.org/3/';
 const apiURL = baseURL + '/discover/movie?sort_by=popularity.desc&' + apiKey;
 const imgURL = 'https://image.tmdb.org/t/p/w500/';
 
+const main = document.getElementById('main');
+
 getMovies(apiURL);
 
 function getMovies(url) {
@@ -13,6 +15,7 @@ function getMovies(url) {
 }
 
 function showMovies(data) {
+    main.innerHTML = '';
 
     data.forEach(movie => {
         const {title, poster_path, vote_average, overview} = movie;
@@ -22,54 +25,25 @@ function showMovies(data) {
             <img src="${imgURL + poster_path}" alt="${title}">
 
             <div class="movie-info">
-                <h3>Movie Title</h3>
-                <span class="green">9.8</span>
+                <h3>${title}</h3>
+                <span class="${getColor(vote_average)}">${vote_average}</span>
             </div>
             <div class="overview">
                 <h3>overview</h3>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Dolorum, neque. Ut ratione excepturi doloremque sint rerum distinctio, earum ea dolorem suscipit enim
-                aut facilis omnis eius ducimus fuga harum obcaecati.
+                ${overview}
             </div>
         `
+
+        main.appendChild(movieEl);
     })
 }
 
-// Global Variables
-// var apiKey = "636459deadfe82e42a5d3a5699ce8a6c";
-// var searchHistoryList = [];
-
-// let baseURL = 'https://api.themoviedb.org/3/';
-// let configData = null;
-// let baseImageURL = null;
-
-// let getConfig = function () {
-//     let url = "".concat(baseURL, 'configuration?api_key=', apiKey);
-//     fetch(url)
-//     .then((result) => {
-//         return result.json();
-//     })
-//     .then((data) => {
-//         baseImageURL = data.images.secure_base_url;
-//         configData = data.images;
-//         console.log('config:', data);
-//         console.log('config fetched');
-//         runSearch('jaws')
-//     })
-//     .catch(function(err) {
-//         alert(err);
-//     });
-// }
-
-// let runSearch = function (keyword) {
-//     let url = ''.concat(baseURL, 'search/movie?api_key=', apiKey, '&query=', keyword);
-//     fetch(url)
-//     .then(result=>result.json())
-//     .then((data)=> {
-//         // Process the returned data
-//         document.getElementById('output').innerHTML = JSON.stringify(data, null, 4);
-//         // work with results array
-//     })
-// }
-
-// document.addEventListener('DOMContentLoaded', getConfig);
+function getColor(vote) {
+    if(vote >= 8) {
+        return 'green'
+    } else if(vote >= 5) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
+}
